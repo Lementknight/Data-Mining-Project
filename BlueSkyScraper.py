40699977339bc40b902171150159bb8ffd66f068
Caleb Aguirre-Leon <caaguirr@g.syr.edu>
2024-12-03 09:59:05 -0500

    Cleaning Up Repo

--- BlueSkyScraper.py
+++ BlueSkyScraper.py
@@ -37,7 +37,6 @@ def create_session():
 
 
 
-# Figure Out
 def search_posts(query, access_token, since, until, limit=25, sort="top"):
     """
     Search for posts using the BlueSky API.
@@ -83,17 +82,12 @@ def extract_post_data(posts):
     :return: List of dictionaries containing post data.
     """
     extracted_data = []
-    # if start_date and end_date:
-    #     start_date = datetime.strptime(start_date, "%Y-%m-%d")
-    #     end_date = datetime.strptime(end_date, "%Y-%m-%d")
 
     for post in posts:
         try:
             post_content = post["record"].get("text", "")  # Extract the text content
             author_handle = post["author"]["handle"]  # Extract the author's handle
             created_at = post["indexedAt"]  # Extract the indexed timestamp
-            # cid = post["cid"]
-            # keys = post.keys()
             postId = post["uri"].split("/")[-1]
             postLink = f"https://bsky.app/profile/{author_handle}/post/{postId}"
 
@@ -108,8 +102,6 @@ def extract_post_data(posts):
                     "author": author_handle,
                     "content": post_content,
                     "created_at": created_at,
-                    # "cid": cid,
-                    # "keys": keys,
                     "postLink": postLink,
                 }
             )
@@ -156,7 +148,7 @@ if __name__ == "__main__":
     print("Extracting post data...")
     post_data = extract_post_data(raw_posts)
 
-    # print("Here is the data:", post_data)
+    print("Here is the data:", post_data)
 
     # Save posts to CSV
     print("Saving posts to CSV...")
